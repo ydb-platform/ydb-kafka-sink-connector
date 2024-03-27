690b303ecdd5bb489c4f87e37f029d4536dffc15
Farid Kurbanov <f.kurbanov120303@yandex.ru>
2024-03-27 20:35:21 +0300

    dev: KafkaSinkConnectorConfig refactoring

--- src/main/java/ydb/kafka/connector/config/KafkaSinkConnectorConfig.java
+++ src/main/java/ydb/kafka/connector/config/KafkaSinkConnectorConfig.java
@@ -7,7 +7,6 @@ import org.apache.kafka.common.config.ConfigDef.Importance;
 
 import java.util.Map;
 
-// fixme не используется
 public class KafkaSinkConnectorConfig extends AbstractConfig {
 
     public static final String SOURCE_TOPIC = "topics";
@@ -33,22 +32,39 @@ public class KafkaSinkConnectorConfig extends AbstractConfig {
     public static final String YDB_DATABASE_DEFAULT_VALUE = "/local";
     public static final String YDB_DATABASE_DOC = "Define YDB sink database name";
 
-    public static final String SINK_SERVER_CONNECTION_STRING = String.format("grpc://%s:%d%s",
-            YDB_HOSTNAME_DEFAULT_VALUE,
-            GRPC_TLS_PORT_DEFAULT_VALUE,
-            YDB_DATABASE_DEFAULT_VALUE);
+    public static final String SINK_SERVER_CONNECTION_STRING = "ydb.connection.string";
+    public static final String SINK_SERVER_CONNECTION_STRING_VALUE =
+            makeConnectionString(
+                    YDB_HOSTNAME_DEFAULT_VALUE,
+                    GRPC_TLS_PORT_DEFAULT_VALUE,
+                    YDB_DATABASE_DEFAULT_VALUE);
+    public static final String SINK_SERVER_CONNECTION_DOC = "YDB connection string";
+
 
     public static ConfigDef CONFIG = new ConfigDef()
-            // TODO разобраться с тем, что будет, если не указать HIGH Importance value в конфиг файле
-            //  и как переопределяются DEFAULT значения
             .define(SOURCE_TOPIC, Type.STRING, SOURCE_TOPIC_DEFAULT_VALUE, Importance.HIGH, SOURCE_TOPIC_DOC)
             .define(SINK_BOOTSTRAP_SERVER, Type.STRING, SINK_BOOTSTRAP_SERVER_DEFAULT_VALUE, Importance.HIGH, SINK_BOOTSTRAP_SERVER_DOC)
             .define(YDB_HOSTNAME, Type.STRING, YDB_HOSTNAME_DEFAULT_VALUE, Importance.HIGH, YDB_HOSTNAME_DOC)
             .define(GRPC_TLS_PORT, Type.INT, GRPC_TLS_PORT_DEFAULT_VALUE, Importance.HIGH, GRPC_TLS_PORT_DOC)
             .define(YDB_DATABASE, Type.STRING, YDB_DATABASE_DEFAULT_VALUE, Importance.HIGH, YDB_DATABASE_DOC)
+            .define(SINK_SERVER_CONNECTION_STRING, Type.STRING, SINK_SERVER_CONNECTION_STRING_VALUE, Importance.HIGH, SINK_SERVER_CONNECTION_DOC)
             ;
 
-    public KafkaSinkConnectorConfig(Map<String, String> props) {
+    public static KafkaSinkConnectorConfig create(Map<String, String> props) {
+        String hostname = props.get(YDB_HOSTNAME);
+        Integer port = Integer.parseInt(props.get(GRPC_TLS_PORT));
+        String databaseName = props.get(YDB_DATABASE);
+
+        props.put(SINK_SERVER_CONNECTION_STRING, makeConnectionString(hostname, port, databaseName));
+
+        return new KafkaSinkConnectorConfig(props);
+    }
+
+    private KafkaSinkConnectorConfig(Map<String, String> props) {
         super(CONFIG, props);
+    }
+
+    private static String makeConnectionString(String hostname, Integer port, String databaseName) {
+        return String.format("grpc://%s:%d%s", hostname, port, databaseName);
     }
 }
