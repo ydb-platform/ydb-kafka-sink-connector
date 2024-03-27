f80b5a590196fb7e0b902d9a126306bfd87838a8
Farid Kurbanov <f.kurbanov120303@yandex.ru>
2024-03-27 20:35:21 +0300

    dev: KafkaSinkConnectorConfig refactoring

--- src/main/java/ydb/kafka/connector/KafkaSinkTask.java
+++ src/main/java/ydb/kafka/connector/KafkaSinkTask.java
@@ -1,18 +1,19 @@
 package ydb.kafka.connector;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import tech.ydb.auth.AuthProvider;
 import tech.ydb.auth.NopAuthProvider;
 import tech.ydb.core.grpc.GrpcTransport;
 import tech.ydb.table.SessionRetryContext;
 import tech.ydb.table.TableClient;
+import tech.ydb.table.description.TableDescription;
 import tech.ydb.table.transaction.TxControl;
+import tech.ydb.table.values.PrimitiveType;
 import ydb.kafka.connector.config.KafkaSinkConnectorConfig;
 import ydb.kafka.connector.utils.DataUtility;
 import ydb.kafka.connector.utils.Record;
@@ -20,10 +21,13 @@ import ydb.kafka.connector.utils.Record;
 import java.text.MessageFormat;
 import java.util.*;
 
+import static ydb.kafka.connector.config.KafkaSinkConnectorConfig.SINK_SERVER_CONNECTION_STRING;
+import static ydb.kafka.connector.config.KafkaSinkConnectorConfig.SOURCE_TOPIC;
+
+@Slf4j
 public class KafkaSinkTask extends SinkTask {
-    private final static Logger log = LoggerFactory.getLogger(KafkaSinkTask.class);
+    private KafkaSinkConnectorConfig config;
     private String connectorName;
-    private String connectionString;
     private GrpcTransport transport;
     private final AuthProvider authProvider = NopAuthProvider.INSTANCE;
     private TableClient tableClient;
@@ -37,17 +41,33 @@ public class KafkaSinkTask extends SinkTask {
     public void start(Map<String, String> props) {
         try {
             log.info("Task config setting : " + props.toString());
-            KafkaSinkConnectorConfig config;
             connectorName = props.get("name");
-            config = new KafkaSinkConnectorConfig(props);
+            config = KafkaSinkConnectorConfig.create(props);
 
-            connectionString = KafkaSinkConnectorConfig.SINK_SERVER_CONNECTION_STRING;
-            transport = GrpcTransport.forConnectionString(connectionString).withAuthProvider(authProvider).build();
+            transport = GrpcTransport.forConnectionString(config.getString(SINK_SERVER_CONNECTION_STRING))
+                    .withAuthProvider(authProvider).build();
             tableClient = TableClient.newClient(transport).build();
-
+            createTable();
         } catch (Exception e) {
             throw new ConnectException(e.getMessage(), e);
         }
+
+    }
+
+    public void createTable() {
+        String sourceTopicName = config.getString(SOURCE_TOPIC);
+        SessionRetryContext retryCtx = SessionRetryContext.create(tableClient).build();
+
+        TableDescription seriesTable = TableDescription.newBuilder()
+                .addNonnullColumn("offset", PrimitiveType.Int64)
+                .addNonnullColumn("partition", PrimitiveType.Int32)
+                .addNullableColumn("key", PrimitiveType.Text)
+                .addNullableColumn("value", PrimitiveType.Text)
+                .setPrimaryKeys("offset", "partition")
+                .build();
+
+        retryCtx.supplyStatus(session -> session.createTable(transport.getDatabase() + "/" + sourceTopicName, seriesTable))
+                .join().expectSuccess("Can't create table /" + sourceTopicName);
 
     }
 
@@ -63,25 +83,23 @@ public class KafkaSinkTask extends SinkTask {
                 }
             }
         }
-
     }
 
     private void saveRecord(Record<String, String> record) {
         String queryTemplate
-                = "UPSERT INTO " + KafkaSinkConnectorConfig.SOURCE_TOPIC_DEFAULT_VALUE +  " (partition, offset, key, value)"
+                = "UPSERT INTO " + config.getString(KafkaSinkConnectorConfig.SOURCE_TOPIC) + " (partition, offset, key, value)"
                 + " VALUES ({0}, {1}, \"{2}\", \"{3}\");";
 
         String query = MessageFormat.format(queryTemplate, record.partition, record.offset, record.key, record.value);
 
         SessionRetryContext retryCtx = SessionRetryContext.create(tableClient).build();
 
-        // Begin new transaction with SerializableRW mode
-        TxControl txControl = TxControl.serializableRw().setCommitTx(true);
+        TxControl<TxControl.TxSerializableRw> txControl = TxControl.serializableRw().setCommitTx(true);
 
-        // Executes data query with specified transaction control settings.
         retryCtx.supplyResult(session -> session.executeDataQuery(query, txControl))
                 .join().getValue();
 
+        log.info(query);
     }
 
     @Override
